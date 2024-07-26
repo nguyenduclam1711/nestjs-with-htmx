f0640c3713477df69bca7b4922af945868fd632a
Lam LUL <nguyenduclam1711@gmail.com>
2024-07-26 16:10:43 +0700

    Add tailwindcss and daisy ui + htmx js

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // use compression for response
+  app.use(compression());
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
