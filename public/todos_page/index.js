65bcd98152f81a7744672e92981960467498d469
Lam LUL <nguyenduclam1711@gmail.com>
2024-07-30 22:12:54 +0700

    Update UI for create todo modal and reset form after submit

--- public/todos_page/index.js
+++ public/todos_page/index.js
@@ -2,11 +2,22 @@ document.body.addEventListener('htmx:afterRequest', function (evt) {
   const parameters = evt?.detail?.requestConfig?.parameters;
   if (parameters) {
     const requestType = parameters.get('requestType');
-    if (requestType === 'DELETE_TODO') {
-      const deleteId = parameters.get('deleteId');
-      const ele = document.getElementById(deleteId);
-      if (ele) {
-        ele.remove();
+    switch (requestType) {
+      case 'DELETE_TODO': {
+        const deleteId = parameters.get('deleteId');
+        const ele = document.getElementById(deleteId);
+        if (ele) {
+          ele.remove();
+        }
+        break;
+      }
+      case 'CREATE_TODO': {
+        const formId = parameters.get('formId');
+        const form = document.getElementById(formId);
+        if (form) {
+          form.reset();
+        }
+        break;
       }
     }
   }
